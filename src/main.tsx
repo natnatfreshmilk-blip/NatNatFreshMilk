b077dcb108b9df9576abdacd9299d421bdb79d18
natnatfreshmilk-blip <natnatfreshmilk@gmail.com>
2026-07-24 12:36:27 +0700

    feat: add favicon to website
    Configure application favicon and implement dynamic loading for branding consistency.

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,20 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import logoImg from './assets/images/logo_1784628827555.jpg';
+
+// Set favicon dynamically using logo image asset
+const setFavicon = (url: string) => {
+  let link = document.querySelector<HTMLLinkElement>("link[rel*='icon']");
+  if (!link) {
+    link = document.createElement('link');
+    link.rel = 'shortcut icon';
+    document.getElementsByTagName('head')[0].appendChild(link);
+  }
+  link.type = 'image/jpeg';
+  link.href = url;
+};
+setFavicon(logoImg);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
